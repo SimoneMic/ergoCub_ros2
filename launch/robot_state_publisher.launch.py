3b10132ef791cf52e0bf5df05fb12b01fd2e6697
Simone Micheletti <simone.micheletti@iit.it>
2022-06-03 16:13:50 +0000

    ergoCub ros2 navigation pkg initial commit

---
+++ launch/robot_state_publisher.launch.py
@@ -0,0 +1,19 @@
+import imp
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+
+    robot_urdf = "home/user1/cer-sim/gazebo/share/iCub/robots/stickBot/model.urdf"
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[robot_urdf]
+    )
+
+    return LaunchDescription(robot_state_publisher_node)
+
