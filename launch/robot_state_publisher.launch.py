8acaf04899e5ef8b1f754b5119f0adf377cc7cca
Simone Micheletti <simone.micheletti@iit.it>
2022-06-09 15:55:03 +0000

    fixed and tested py launch file robot_state_publisher

--- launch/robot_state_publisher.launch.py
+++ launch/robot_state_publisher.launch.py
@@ -1,4 +1,5 @@
 import imp
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
@@ -6,14 +7,29 @@ from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
 
-    robot_urdf = "$(command 'cat /home/user1/robotology-superbuild/build/install/share/iCub/robots/stickBot/model.urdf')"
+    robot_urdf = '/home/user1/robotology-superbuild/build/install/share/iCub/robots/stickBot/model.urdf'
+    with open(robot_urdf, 'r') as infp:
+        robot_desc = infp.read()
 
-    robot_state_publisher_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        parameters=[robot_urdf]
-    )
-
-    return LaunchDescription(robot_state_publisher_node)
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='true',
+            description='Use simulation (Gazebo) clock if true'),
+            
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': robot_desc , 'use_sim_time': use_sim_time}]),
+            
+        Node(
+            package='ergoCub_ros2',
+            executable='odometry_standalone',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}]
+        )
+    ])
 
