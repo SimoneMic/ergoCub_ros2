bb6bf027106b4866d89a9a8db23555f0bc789e92
Simone Micheletti <simone.micheletti@iit>
2022-06-13 11:59:41 +0000

    added chest_projection launch file

---
+++ launch/chest_projection.launch.py
@@ -0,0 +1,26 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='true',
+            description='Use simulation (Gazebo) clock if true'),
+        Node(
+            package='ergoCub_ros2',
+            executable='chest_projection',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}]
+        )
+    ])
