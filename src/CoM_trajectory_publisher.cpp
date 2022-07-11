b99b5eecfabd80828b248afb2396b18ff753ee85
Simone Micheletti <simone.micheletti@iit.it>
2022-07-11 10:21:51 +0000

    initial (preliminiary) commit of features extraction from CoM trajectory

--- src/CoM_trajectory_publisher.cpp
+++ src/CoM_trajectory_publisher.cpp
@@ -26,6 +26,12 @@ private:
 
     void callback()
     {
+        //Feet pos tracker and notable points
+        nav2_msgs::msg::Path left_extremes;
+        nav2_msgs::msg::Path right_extremes;
+        nav2_msgs::msg::Path extremes_points;   
+        nav2_msgs::msg::Path center_points;
+        //Std
         yarp::os::Bottle msg_in;
         nav_msgs::msg::Path CoM_path;
         nav_msgs::msg::Path downsampled_CoM_path;
@@ -47,6 +53,19 @@ private:
         pose_tmp.header.stamp = now();
         pose_tmp.header.frame_id = "odom";
         tf2::Quaternion q;
+
+        //Variables for finding local extremes on the left and rigt of the CoM path
+        geometry_msgs::msg::PoseStamped max_pose;   //right extreme
+        max_pose.pose.position.z = 0.0;
+        max_pose.header = pose_tmp.header;
+        geometry_msgs::msg::PoseStamped min_pose;   //left extreme
+        min_pose.pose.position.z = 0.0;
+        min_pose.header = pose_tmp.header;
+        //Interpolated Centers of the CoM trajectory
+        geometry_msgs::msg::PoseStamped interpolated_center;   //left extreme
+        interpolated_center.pose.position.z = 0.0;
+        interpolated_center.header = pose_tmp.header;
+        //Loop through the CoM message (x_pose1 y_pose1 tetha_pose1 x_pose2 y_pose2 ...)
         for(int i=0; i<n ; i+=3)
         {
             q.setRPY(0, 0, msg_in.get(i+2).asFloat64());
@@ -57,25 +76,67 @@ private:
             pose_tmp.pose.position.x = msg_in.get(i).asFloat64();
             pose_tmp.pose.position.y = msg_in.get(i+1).asFloat64();
             CoM_path.poses.push_back(pose_tmp);
-            //DOWNSAMPLE
+            //DOWNSAMPLE the CoM path
             if (downsampled_CoM_path.poses.empty())     //is it the first pose?
             {
                 downsampled_CoM_path.poses.push_back(pose_tmp);
+                //notable points -> start pose
+                center_points.poses.push_back(pose_tmp);
+                extremes_points.poses.push_back(pose_tmp);
             }
             else if (i+3 == n && i!=0)                   //is it the last pose?
             {
                 downsampled_CoM_path.poses.push_back(pose_tmp);
+                center_points.poses.push_back(pose_tmp);
+                extremes_points.poses.push_back(pose_tmp);
             }
             else
             {
+                //Distance check for downsampling
                 double poses_distance = sqrt(std::pow(downsampled_CoM_path.poses.back().pose.position.x - CoM_path.poses.back().pose.position.x, 2) + 
                                         std::pow(downsampled_CoM_path.poses.back().pose.position.y - CoM_path.poses.back().pose.position.y, 2));
                 if (poses_distance >= 0.01 )
                 {
                     downsampled_CoM_path.poses.push_back(pose_tmp);
                 }
+
+                //Check for notable poses: extremes
+                geometry_msgs::msg::PoseStamped prev_pose;  //could use a float or double
+                //prev_pose.pose.position.x = msg_in.get(i-3).asFloat64();
+                prev_pose.pose.position.y = msg_in.get(i-2).asFloat64();
+                //How many positions are considered? check a neighbourhood of 2 - 3 - 5 poses?
+                //Only on Y axes (horizontal)?
+                //RIGHT EXTREMES
+                if ((pose_tmp.pose.position.y - prev_pose.pose.position.y) > 0)
+                {
+                    max_pose.pose.position = pose_tmp.pose.position;
+                }
+                else 
+                {
+                    right_extremes.poses.push_back(max_pose);
+                }
+
+                //LEFT EXTREMES
+                if ((pose_tmp.pose.position.y - prev_pose.pose.position.y) < 0)
+                {
+                    min_pose.pose.position = pose_tmp.pose.position;
+                }
+                else 
+                {
+                    left_extremes.poses.push_back(min_pose);
+                }
+            }
+
+            //Once I've found the extremes I look for the points on the median between two consecutive extremes
+            //Y position gives the order on what foot went first
+            if (!left_extremes.poses.empty() && !right_extremes.poses.empty())
+            {
+                double interpolated_center_y = right_extremes.poses.back().pose.position.y - left_extremes.poses.back().pose.position.y;
+                double interpolated_center_x = right_extremes.poses.back().pose.position.x - left_extremes.poses.back().pose.position.x;
             }
         }
+ 
+
         RCLCPP_INFO(this->get_logger(), "Downsized CoM trajectory (%i) \n", downsampled_CoM_path.poses.size());
         pub_->publish(CoM_path);
         downsampled_pub_->publish(downsampled_CoM_path);
