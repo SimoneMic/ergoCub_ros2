e6aa5ca6da740c25061e87e8630861d86e1b84cf
Simone Micheletti <simone.micheletti@iit.it>
2022-07-13 16:13:20 +0000

    lowered loop freq

--- src/chest_projection.cpp
+++ src/chest_projection.cpp
@@ -21,7 +21,7 @@ private:
     /* const */
     const char* reader_port_name = "/chest_projector/wrench_reader:i";
     const char* writer_port_name = "/base-estimator/contacts/stateAndNormalForce:o";
-    const double loopFreq = 50.0;
+    const double loopFreq = 30.0;
     const char* chest_link = "chest";
     /* msgs */
     geometry_msgs::msg::TransformStamped TF;
