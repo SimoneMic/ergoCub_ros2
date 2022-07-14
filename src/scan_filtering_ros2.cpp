c2cec85db8120f5bc5da0b3f256b602e0456d851
Simone Micheletti <simone.micheletti@iit.it>
2022-07-14 13:46:13 +0000

    reduced TF tolerance

--- src/scan_filtering_ros2.cpp
+++ src/scan_filtering_ros2.cpp
@@ -39,7 +39,7 @@ private:
             scan_in->header.frame_id,
             referece_frame,
             tf2_ros::fromMsg(scan_in->header.stamp) + tf2::durationFromSec(scan_in->ranges.size() * scan_in->time_increment),
-            tf2::durationFromSec(0.5),
+            tf2::durationFromSec(0.1),  //0.5
             & transform_error ))
         {
             // Converts the scans into cartesian space points
@@ -51,7 +51,7 @@ private:
             pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered2 (new pcl::PointCloud<pcl::PointXYZ>);
             // Pass data from Pointcloud2 to pcl
             pcl::fromROSMsg(cloud, *pcl_cloud);
-            RCLCPP_INFO(get_logger(), "Original cloud size: %li \n", pcl_cloud->size());
+            //RCLCPP_INFO(get_logger(), "Original cloud size: %li \n", pcl_cloud->size());
             // FILTER 1 - PASSTHROUGH
             pcl::PassThrough<pcl::PointXYZ> pass;       // Create the passthrough filtering object
             pass.setInputCloud (pcl_cloud);
@@ -59,7 +59,7 @@ private:
             pass.setFilterLimits(filter_z_low, filter_z_high);
             // Filtering
             pass.filter (*cloud_filtered1);
-            RCLCPP_INFO(get_logger(), "Cloud size after filter 1: %li \n", cloud_filtered1->size());
+            //RCLCPP_INFO(get_logger(), "Cloud size after filter 1: %li \n", cloud_filtered1->size());
             // FILTER 2 - PLANE PROJECTOR
             pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients ());   // Create a set of planar coefficients with X=Y=0,Z=1, d= 1.5
             coefficients->values.resize(4);
