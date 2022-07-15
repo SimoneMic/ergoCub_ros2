b56372e33ef7d787dc0e99549a77e27a9efaa2ef
Simone Micheletti <simone.micheletti@iit.it>
2022-07-15 15:12:34 +0000

    goal path now is sent only when the robot is in double support. Reading feet sensors values to enstablish when in double support

--- src/setpoint_converter.cpp
+++ src/setpoint_converter.cpp
@@ -14,17 +14,78 @@
 using namespace std::chrono_literals;
 using std::placeholders::_1;
 
+//Class used for YARP port callbacks
+class YarpFeetDataProcessor : public yarp::os::PortReader
+{
+private:
+    /* data */
+    yarp::os::Bottle transformed_goal_;
+    bool on_double_support;     //flag for when the robot enters on double support
+    yarp::os::Port port;
+
+    const double sensor_threshold = 100.0;
+
+    const std::string port_name = "/setopint_converter/talker:o";
+    const std::string server_name = "/walking-coordinator/goal:i";
+public:
+    YarpFeetDataProcessor()
+    {
+        on_double_support = false;
+
+        port.open(port_name);
+        yarp::os::Network::connect(port_name, server_name);     //todo - check for connection or errors
+    };
+
+    void storePath(yarp::os::Bottle &msg)
+    {
+        transformed_goal_ = msg;
+    }
+
+    bool read(yarp::os::ConnectionReader& connection) override
+    {
+        yarp::os::Bottle b;
+        bool ok = b.read(connection);
+        if (!ok) {
+            return false;
+        }
+        //Condition for double support
+        if (b.get(0).asFloat64() > sensor_threshold && b.get(1).asFloat64() > sensor_threshold)
+        {
+            //if I wasn't on double support before I can send data -> this means that it's the first time
+            if (!on_double_support)
+            {
+                on_double_support = true;
+
+                //check for data sanity before transmitting it
+                //todo
+                port.write(transformed_goal_);  //send data only once per double support
+            }
+        }
+        else
+        {
+            on_double_support = false;
+        }
+        
+        return true;
+    }
+};
+
+
+
+
 class SetpointConverter : public rclcpp::Node
 {
 private:
     const std::string topic_name = "/plan";     //subscribe to global plan
-    const char* port_name = "/setopint_converter/talker:o";
-    const char* server_name = "/walking-coordinator/goal:i";
+    //const char* port_name = "/setopint_converter/talker:o";
+    //const char* server_name = "/walking-coordinator/goal:i";
     //const double x_vel_min = 0.04;
     //const double x_vel_max = 0.2;
     //const double y_vel_min = 0.05;
     //const double y_vel_max = 0.6;
-    yarp::os::Port port;
+    //yarp::os::Port port;
+    yarp::os::Port feet_state_port;
+    const std::string feet_state_port_name = "/setopint_converter/feet_state:i";
     std::shared_ptr<tf2_ros::TransformListener> tf_listener_{nullptr};
     std::unique_ptr<tf2_ros::Buffer> tf_buffer_in;
     
@@ -32,7 +93,7 @@ private:
     //double setpoint_y = 0;
 
     rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr setpoint_sub_;
-    
+    YarpFeetDataProcessor processor;
     /*
     void msg_callback(const geometry_msgs::msg::Twist::ConstPtr& msg_in)
     {
@@ -69,13 +130,16 @@ private:
 
    void msg_callback(const nav_msgs::msg::Path::ConstPtr& msg_in)
     {
+
         yarp::os::Bottle msg_out;
         // Need to transform the path from map frame to robot frame
         geometry_msgs::msg::PoseStamped goal_robot;
         //tf_buffer_in->transform(msg_in->poses.back(), goal_robot, "chest", 200ms);    //goal pose in robot frame: could throw exception for extrapolation into the future
         try
         {
-            geometry_msgs::msg::TransformStamped  TF = tf_buffer_in->lookupTransform("chest", msg_in->header.frame_id, rclcpp::Time(0), 300ms);
+            geometry_msgs::msg::TransformStamped  TF = tf_buffer_in->lookupTransform("root_link", msg_in->header.frame_id, rclcpp::Time(0), 100ms);
+            TF.transform.translation.x += 0.1;  //offsetted reference point used by the walking-controller
+            //todo: implement a slicing of the path
             tf2::doTransform(msg_in->poses.back(), goal_robot, TF);
         }
         catch(const std::exception& e)
@@ -85,16 +149,16 @@ private:
         msg_out.addFloat64(goal_robot.pose.position.x);
         msg_out.addFloat64(goal_robot.pose.position.y);
         RCLCPP_INFO(this->get_logger(), "Setpoint x: %f y: %f \n", msg_out.get(0).asFloat64(), msg_out.get(1).asFloat64());
-
-        port.write(msg_out);
+        //I should publish the command only when the robot is in double support -> other callback
+        //port.write(msg_out);
+        processor.storePath(msg_out);
     }
 public:
     SetpointConverter() : rclcpp::Node("setpoint_converter_node")
     {
         // Network Setup
         yarp::os::Network yarp;
-        port.open(port_name);
-        yarp::os::Network::connect(port_name, server_name);     //todo - check for connection or errors
+        
         setpoint_sub_ = this->create_subscription<nav_msgs::msg::Path>(
             topic_name,
             10,
@@ -103,6 +167,11 @@ public:
 
         tf_buffer_in = std::make_unique<tf2_ros::Buffer>(this->get_clock());
         tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_in);
+
+        //Create YarpFeetDataProcessor object
+        feet_state_port.open(feet_state_port_name);
+        yarp::os::Network::connect("/base-estimator/contacts/stateAndNormalForce:o", feet_state_port_name);
+        feet_state_port.setReader(processor);
     }
 };
 
