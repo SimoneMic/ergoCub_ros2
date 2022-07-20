538c0a26776258c6c27f9e3ccb83282190c92817
Simone Micheletti <simone.micheletti@iit.it>
2022-07-20 15:02:35 +0000

    Changed setpoint_converter to now transform the path just before sending the goal.

--- src/setpoint_converter.cpp
+++ src/setpoint_converter.cpp
@@ -19,10 +19,10 @@ class YarpFeetDataProcessor : public yarp::os::PortReader
 {
 private:
     /* data */
-    yarp::os::Bottle transformed_goal_;
-    bool on_double_support;     //flag for when the robot enters on double support
+    nav_msgs::msg::Path::ConstPtr path_msg;
+    //bool on_double_support;     //flag for when the robot enters on double support
     yarp::os::Port port;
-
+    std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
     const double sensor_threshold = 100.0;
 
     const std::string port_name = "/setopint_converter/talker:o";
@@ -32,16 +32,20 @@ private:
 public:
     YarpFeetDataProcessor()
     {
-        on_double_support = false;
+        //on_double_support = false;
         send_goal = false;
         port.open(port_name);
         yarp::os::Network::connect(port_name, server_name);     //todo - check for connection or errors
     };
 
-    void storePath(yarp::os::Bottle &msg)
+    void init(std::shared_ptr<tf2_ros::Buffer> &buffer)
     {
-        transformed_goal_ = msg;
-        send_goal = true;
+        tf_buffer_ = buffer;
+    }
+
+    void storePath(const nav_msgs::msg::Path::ConstPtr& msg)
+    {
+        path_msg = msg;
     }
 
     //set the internal flag to whether send the goal or not
@@ -61,32 +65,35 @@ public:
         //Condition for double support -> Should also check if in the middle of the CoM path during oscillation???
         if (b.get(0).asFloat64() > sensor_threshold && b.get(1).asFloat64() > sensor_threshold)
         {
-            
-            //if I wasn't on double support before I can send data -> this means that it's the first time
-            if (!on_double_support)
+            //check for data sanity before transmitting it
+            //todo
+            if (send_goal)  //This means that I have a new path
             {
-                on_double_support = true;
-                std::cout << "On Double Support! \n";
-                //check for data sanity before transmitting it
-                //todo
-                if (send_goal)
+                //I have to transform the goal to the robot reference frame (root_link) only when I have to send the data
+                yarp::os::Bottle msg_out;
+                geometry_msgs::msg::PoseStamped goal_robot;
+                goal_robot.pose.position.x = 0;
+                goal_robot.pose.position.y = 0;
+                try
                 {
-                    std::cout << "Passing Goal \n";
-                    port.write(transformed_goal_);  //send data only once per double support
-                    send_goal = false;
+                    //
+                    geometry_msgs::msg::TransformStamped  TF = tf_buffer_->lookupTransform("root_link", path_msg->header.frame_id, rclcpp::Time(0), 100ms);
+                    TF.transform.translation.x += 0.1;  //offsetted reference point used by the walking-controller -> found in config file by person distance
+                    //todo: implement a slicing of the path
+                    tf2::doTransform(path_msg->poses.back(), goal_robot, TF);
                 }
-            }
-            else if (on_double_support && send_goal)
-            {
-                std::cout << "Passing Goal \n";
-                port.write(transformed_goal_);  //send data only once per double support
+                catch(const std::exception& e)
+                {
+                    std::cerr << "Transform Exception: " << e.what() << "\n";
+                    goal_robot.pose.position.x = 0;
+                    goal_robot.pose.position.y = 0;
+                }
+                msg_out.addFloat64(goal_robot.pose.position.x);
+                msg_out.addFloat64(goal_robot.pose.position.y);
+                std::cout << "Passing Setpoint x: " << msg_out.get(0).asFloat64() << " y: " << msg_out.get(1).asFloat64() << "\n";
+                port.write(msg_out);  //send data only once per double support
                 send_goal = false;
             }
-        }
-        else
-        {
-            std::cout << "Not in double Support \n";
-            on_double_support = false;
         }
         return true;
     }
@@ -99,58 +106,18 @@ private:
     const std::string topic_name = "/plan";     //subscribe to global plan
     //const char* port_name = "/setopint_converter/talker:o";
     //const char* server_name = "/walking-coordinator/goal:i";
-    //const double x_vel_min = 0.04;
-    //const double x_vel_max = 0.2;
-    //const double y_vel_min = 0.05;
-    //const double y_vel_max = 0.6;
     //yarp::os::Port port;
     yarp::os::Port feet_state_port;
     const std::string feet_state_port_name = "/setopint_converter/feet_state:i";
     std::shared_ptr<tf2_ros::TransformListener> tf_listener_{nullptr};
-    std::unique_ptr<tf2_ros::Buffer> tf_buffer_in;
-    
-    //double setpoint_x = 0;
-    //double setpoint_y = 0;
+    std::shared_ptr<tf2_ros::Buffer> tf_buffer_in;
 
     rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr setpoint_sub_;
     YarpFeetDataProcessor processor;
-    /*
-    void msg_callback(const geometry_msgs::msg::Twist::ConstPtr& msg_in)
-    {
-        //SPEED CONVERTER
-        // X
-        if (msg_in->linear.x >= x_vel_min && msg_in->linear.x <= x_vel_max)    
-            //setpoint_x = msg_in->linear.x * 5;
-            setpoint_x = 1;
-        else if (msg_in->linear.x > 0 && msg_in->linear.x < x_vel_min)
-            setpoint_x = 0.2;
-        else if (msg_in->linear.x > x_vel_max)
-            setpoint_x = 1;
-        else
-            setpoint_x = 0;     //for safety
-
-        // Y
-        if (abs(msg_in->angular.z) >= y_vel_min && abs(msg_in->angular.z) <= y_vel_max)
-            setpoint_y =  msg_in->angular.z * 2;
-        else if (abs(msg_in->angular.z) > 0 && abs(msg_in->angular.z) < y_vel_min)
-            setpoint_y =  0.1 * (msg_in->angular.z / abs(msg_in->angular.z));
-        else if (abs(msg_in->angular.z) > y_vel_max)
-            setpoint_y = (msg_in->angular.z / abs(msg_in->angular.z));
-        else
-            setpoint_y = 0;     //for safety
-
-        yarp::os::Bottle msg_out;
-        msg_out.addFloat64(setpoint_x);
-        msg_out.addFloat64(setpoint_y);
-        RCLCPP_INFO(this->get_logger(), "Converting cmd_vel from x: %f yaw: %f ---> x: %f y: %f \n", msg_in->linear.x, msg_in->angular.z, setpoint_x, setpoint_y);
-
-        port.write(msg_out);
-    }
-    */
 
    void msg_callback(const nav_msgs::msg::Path::ConstPtr& msg_in)
     {
-
+        /*
         yarp::os::Bottle msg_out;
         // Need to transform the path from map frame to robot frame
         geometry_msgs::msg::PoseStamped goal_robot;
@@ -169,10 +136,11 @@ private:
         }
         msg_out.addFloat64(goal_robot.pose.position.x);
         msg_out.addFloat64(goal_robot.pose.position.y);
-        RCLCPP_INFO(this->get_logger(), "Setpoint x: %f y: %f \n", msg_out.get(0).asFloat64(), msg_out.get(1).asFloat64());
+        RCLCPP_INFO(this->get_logger(), "STORING Setpoint x: %f y: %f \n", msg_out.get(0).asFloat64(), msg_out.get(1).asFloat64());
         //I should publish the command only when the robot is in double support -> other callback
         //port.write(msg_out);
-        processor.storePath(msg_out);
+        */
+        processor.storePath(msg_in);
         processor.set_permission(true);
     }
 public:
@@ -187,10 +155,11 @@ public:
             std::bind(&SetpointConverter::msg_callback, this, _1)
         );
 
-        tf_buffer_in = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+        tf_buffer_in = std::make_shared<tf2_ros::Buffer>(this->get_clock());    //share to the yarp port
         tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_in);
 
         //Create YarpFeetDataProcessor object
+        processor.init(tf_buffer_in);
         feet_state_port.open(feet_state_port_name);
         yarp::os::Network::connect("/base-estimator/contacts/stateAndNormalForce:o", feet_state_port_name);
         feet_state_port.setReader(processor);
