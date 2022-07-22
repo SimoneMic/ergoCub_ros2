2099a5d3997213bea374e2ddac554dde5bf715fc
Simone Micheletti <simone.micheletti@iit.it>
2022-07-22 12:31:28 +0000

    moved from transformLaserScanToPointCloud to projectLaser method for laser projection to pc2 since the first method should be used on fixed frame (as per documentation says)

--- src/scan_filtering_ros2_compensated.cpp
+++ src/scan_filtering_ros2_compensated.cpp
@@ -3,8 +3,9 @@
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/buffer.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <tf2/transform_datatypes.h>
+#include <tf2_sensor_msgs/tf2_sensor_msgs.h>
 #include "laser_geometry/laser_geometry.hpp"
-#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_types.h>
@@ -31,7 +32,7 @@ private:
     geometry_msgs::msg::TransformStamped compensation_TF;
     std::shared_ptr<tf2_ros::TransformListener> tf_listener_{nullptr};
     std::unique_ptr<tf2_ros::Buffer> tf_buffer_in;
-    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pointcloud_pub;
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pointcloud_pub, debug_pub;
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr raw_scan_sub;
 
     void scan_callback(const sensor_msgs::msg::LaserScan::ConstPtr& scan_in)
@@ -41,38 +42,46 @@ private:
             scan_in->header.frame_id,
             referece_frame,
             tf2_ros::fromMsg(scan_in->header.stamp) + tf2::durationFromSec(scan_in->ranges.size() * scan_in->time_increment),
-            tf2::durationFromSec(0.1),  //0.5
+            tf2::durationFromSec(0.1),  
             & transform_error ))
         {
             // Converts the scans into cartesian space points
-            sensor_msgs::msg::PointCloud2 cloud;
-            projector_.transformLaserScanToPointCloud(referece_frame, *scan_in, cloud, *tf_buffer_in);
+            sensor_msgs::msg::PointCloud2 original_cloud;
+            sensor_msgs::msg::PointCloud2 transformed_cloud;
+            //projector_.transformLaserScanToPointCloud(referece_frame, *scan_in, cloud, *tf_buffer_in); //-> by documentation should be used with fixed frame
+            projector_.projectLaser(*scan_in, original_cloud);
+            //transform cloud from lidar frame to virtual_unicycle_base
+            try
+            {
+                transformed_cloud = tf_buffer_in->transform(original_cloud, referece_frame, tf2::durationFromSec((0.1)));
+            }
+            catch(const std::exception& e)
+            {
+               RCLCPP_ERROR(this->get_logger(), "Cannot transform %s from lidar frame to virtual_unicycle_base: %s \n", original_cloud.header.frame_id, e.what());
+               return;
+            }
             //PCL Clouds
             pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_cloud (new pcl::PointCloud<pcl::PointXYZ>);
             pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_compensated_cloud (new pcl::PointCloud<pcl::PointXYZ>);
             pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered1 (new pcl::PointCloud<pcl::PointXYZ>);
             pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered2 (new pcl::PointCloud<pcl::PointXYZ>);
-
-            //Compensate Cloud
+            //Compensate Cloud by the Y component of the head oscillation during walking
             try
             {
                 compensation_TF = tf_buffer_in -> lookupTransform("virtual_unicycle_base", "lidar", rclcpp::Time(0), rclcpp::Duration::from_seconds(0.1));
                 // Pass data from Pointcloud2 to pcl
-                pcl::fromROSMsg(cloud, *pcl_compensated_cloud);
-                
+                pcl::fromROSMsg(transformed_cloud, *pcl_compensated_cloud);
+                //for each point subtract the y-component of the distance between the lidar and the virtual_unicycle_base
                 for(int i=0; i < pcl_compensated_cloud->points.size(); ++i) //should be optimized
                 {
                     pcl_compensated_cloud->points[i].y -= compensation_TF.transform.translation.y;
                 }
-
-                //tf2::doTransform(*pcl_compensated_cloud, *pcl_cloud, compensation_TF);  //maybe unnecessary (only for height fixing and frame name) -> needed afterwards for filtering
             }
             catch(const std::exception& e)
             {
                RCLCPP_ERROR(this->get_logger(), "Cannot Compensate lidar cloud: %s \n", e.what());
-               pcl::fromROSMsg(cloud, *pcl_compensated_cloud);  //reset
+               pcl::fromROSMsg(transformed_cloud, *pcl_compensated_cloud);  //reset
             }
-
             // Filter cloud
             RCLCPP_INFO(get_logger(), "Original cloud size: %li \n", pcl_compensated_cloud->size());
             // FILTER 1 - PASSTHROUGH
@@ -82,6 +91,9 @@ private:
             pass.setFilterLimits(filter_z_low, filter_z_high);
             // Filtering
             pass.filter (*cloud_filtered1);
+            sensor_msgs::msg::PointCloud2 ros_cloud_debug;
+            pcl::toROSMsg(*cloud_filtered2, ros_cloud_debug);
+            debug_pub->publish(ros_cloud_debug);
             RCLCPP_INFO(get_logger(), "Cloud size after filter 1: %li \n", cloud_filtered1->size());
             // FILTER 2 - PLANE PROJECTOR
             pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients ());   // Create a set of planar coefficients with X=Y=0,Z=1, d= 1.5
@@ -113,6 +125,7 @@ public:
     ScanFilter() : Node("scan_compensating_node")
     {
         pointcloud_pub = this->create_publisher<sensor_msgs::msg::PointCloud2>(pub_topic, 10);
+        debug_pub = this->create_publisher<sensor_msgs::msg::PointCloud2>("debug_pc2", 10);
         tf_buffer_in = std::make_unique<tf2_ros::Buffer>(this->get_clock());
         tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_in);
 
