d026e2728097018bc56b710c3c6aac4152eb245f
Simone <simone_micheletti@outlook.it>
2022-12-06 17:35:30 +0000

    added monitoring of goal reached state to stop the robot

--- src/async_walking_controller_talker.cpp
+++ src/async_walking_controller_talker.cpp
@@ -8,6 +8,7 @@
 #include "yarp/os/Portable.h"
 
 #include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/bool.hpp"
 #include "nav_msgs/msg/path.hpp"
 #include "nav_2d_msgs/msg/pose2_d_stamped.hpp"
 #include "nav_2d_msgs/msg/path2_d.hpp"
@@ -23,50 +24,97 @@
 using namespace std::chrono_literals;
 using std::placeholders::_1;
 
-
 //This class subscribes to the /path topic and will pass it to the walking-controller on a yarp port
 class WalkingControllerTalker : public rclcpp::Node
 {
 private:
     const double zero_speed_threshold = 1e-03;
-    const std::string m_topic_name = "/plan";    
+    const std::string m_topic_name = "/plan";
+    const std::string m_state_topic = "/is_goal_reached/goal_state";
     yarp::os::Port m_feet_state_port;
     const std::string m_outPortName = "/path_converter/path:o";
     const std::string m_inPortName = "/walking-coordinator/goal:i";
     const std::string m_reference_frame = "virtual_unicycle_base";
     yarp::os::BufferedPort<yarp::sig::VectorOf<double>> m_port;
     rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr m_setpoint_sub;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr m_state_sub;
 
     std::shared_ptr<tf2_ros::TransformListener> m_tf_listener{nullptr};
     std::shared_ptr<tf2_ros::Buffer> m_tf_buffer;
+    std::mutex m_mutex;
+    bool m_goalReached = false;
 
     //Each time I have a new path we transform the path and pass it to the walking controller
     void msg_callback(const nav_msgs::msg::Path::ConstPtr& msg_in)
     {
-        //geometry_msgs::msg::TransformStamped TF = m_tf_buffer->lookupTransform("projection", msg_in->header.frame_id, rclcpp::Time(0), 50ms);
-        geometry_msgs::msg::TransformStamped TF = m_tf_buffer->lookupTransform(m_reference_frame, msg_in->header.frame_id, rclcpp::Time(0));
-        TF.transform.translation.x += 0.1;  //offsetted reference point used by the walking-controller -> found in config file by person distance
-        nav_msgs::msg::Path transformed_plan = transformPlan(msg_in, TF, true);
-        if (transformed_plan.poses.size()>0)
+        std::lock_guard<std::mutex> lock(m_mutex);
+        if (!m_goalReached)
         {
-            //std::cout << "Creating port buffer" << std::endl;
-            //Convert Path to yarp vector
+            geometry_msgs::msg::TransformStamped TF = m_tf_buffer->lookupTransform(m_reference_frame, msg_in->header.frame_id, rclcpp::Time(0));
+            TF.transform.translation.x += 0.1;  //offsetted reference point used by the walking-controller -> found in config file by person distance
+            nav_msgs::msg::Path transformed_plan = transformPlan(msg_in, TF, true);
+            if (transformed_plan.poses.size()>0)
+            {
+                //std::cout << "Creating port buffer" << std::endl;
+                //Convert Path to yarp vector
+                auto& out = m_port.prepare();
+                //std::cout << "Clearing port buffer" << std::endl;
+                out.clear();
+                for (int i = 0; i < transformed_plan.poses.size(); ++i)
+                {
+                    out.push_back(transformed_plan.poses.at(i).pose.position.x);
+                    out.push_back(transformed_plan.poses.at(i).pose.position.y);
+                    std::cout << "Passing Path i-th element: " << i << " X : " << out[2*i] << " Y: " << out[2*i+1] << std::endl;
+                }
+
+                //std::cout << "Writing port buffer" << std::endl;
+                m_port.write();
+            }
+            else
+            {
+                std::cout << "Returned empty transformed path" << std::endl;
+            }
+        }
+        else
+        {
+            //Write a path with all 0 poses
             auto& out = m_port.prepare();
             //std::cout << "Clearing port buffer" << std::endl;
             out.clear();
-            for (int i = 0; i < transformed_plan.poses.size(); ++i)
+            for (int i = 0; i < 3; ++i)
             {
-                out.push_back(transformed_plan.poses.at(i).pose.position.x);
-                out.push_back(transformed_plan.poses.at(i).pose.position.y);
+                out.push_back(0.0);
+                out.push_back(0.0);
                 std::cout << "Passing Path i-th element: " << i << " X : " << out[2*i] << " Y: " << out[2*i+1] << std::endl;
             }
-            
+            //std::cout << "Writing port buffer" << std::endl;
+            m_port.write();
+        }
+    }
+
+    //Each time I have a new path we transform the path and pass it to the walking controller
+    void state_callback(const std_msgs::msg::Bool::ConstPtr& in)
+    {
+        if (in->data)
+        {
+            std::lock_guard<std::mutex> lock(m_mutex);
+            m_goalReached = true;
+            //Write a path with all 0 poses
+            auto& out = m_port.prepare();
+            //std::cout << "Clearing port buffer" << std::endl;
+            out.clear();
+            for (int i = 0; i < 3; ++i)
+            {
+                out.push_back(0.0);
+                out.push_back(0.0);
+                std::cout << "Passing Path i-th element: " << i << " X : " << out[2*i] << " Y: " << out[2*i+1] << std::endl;
+            }
             //std::cout << "Writing port buffer" << std::endl;
             m_port.write();
         }
         else
         {
-            std::cout << "Returned empty transformed path" << std::endl;
+            m_goalReached = false;
         }
     }
 
@@ -119,6 +167,12 @@ public:
             m_topic_name,
             10,
             std::bind(&WalkingControllerTalker::msg_callback, this, _1)
+        );
+
+        m_state_sub = this->create_subscription<std_msgs::msg::Bool>(
+            m_state_topic,
+            10,
+            std::bind(&WalkingControllerTalker::state_callback, this, _1)
         );
 
         // TFs
