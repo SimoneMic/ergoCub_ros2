7b5a03059cf65049bd8282b06afbc3ece3803ff0
Simone <simone_micheletti@outlook.it>
2023-02-21 18:33:53 +0000

    removed filtering of the back of the lidar. added rviz config for ergocub and changed path to urdf for robot state pub

--- src/scan_filtering_ros2_compensated.cpp
+++ src/scan_filtering_ros2_compensated.cpp
@@ -76,13 +76,13 @@ private:
             // Filtering
             pass.filter (*cloud_filtered1);
             // FILTER 1B removing the back part of Lidar (simulating the support crane on the real robot)
-            pcl::PassThrough<pcl::PointXYZ> pass_back;       
-            pass_back.setInputCloud (cloud_filtered1);
-            pass_back.setFilterFieldName ("x");
-            pass_back.setFilterLimits(0.0, 25.0);
+            //pcl::PassThrough<pcl::PointXYZ> pass_back;       
+            //pass_back.setInputCloud (cloud_filtered1);
+            //pass_back.setFilterFieldName ("x");
+            //pass_back.setFilterLimits(0.0, 25.0);
             // Filtering
-            pass_back.filter (*cloud_filtered1);
-            sensor_msgs::msg::PointCloud2 ros_cloud_debug;
+            //pass_back.filter (*cloud_filtered1);
+            //sensor_msgs::msg::PointCloud2 ros_cloud_debug;
             //pcl::toROSMsg(*cloud_filtered2, ros_cloud_debug);
             //m_debug_pub->publish(ros_cloud_debug);
             //RCLCPP_INFO(get_logger(), "Cloud size after filter 1: %li \n", cloud_filtered1->size());
